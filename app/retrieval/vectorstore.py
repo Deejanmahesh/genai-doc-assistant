65ccb578006ec3d75a58875c81f2811fe1bc89bc
Deejan Mahesh <deejanmahesh003@gmail.com>
2026-08-18 17:16:55 +0530

    Switch to HuggingFace Inference API to reduce memory usage

--- app/retrieval/vectorstore.py
+++ app/retrieval/vectorstore.py
@@ -1,9 +1,11 @@
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+import os
 
 def build_vectorstore(chunks, persist_dir="./chroma_db"):
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=os.getenv("HF_TOKEN")
     )
-    vector_db = Chroma.from_documents(chunks, embeddings, persist_directory=persist_dir)
-    return vector_db
+    vectordb = Chroma.from_documents(chunks, embeddings, persist_directory=persist_dir)
+    return vectordb
